74aa3be0b1d947f16e228cabccfd2b1e33fb4a5d
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2024-09-10 17:59:09 +0300

    Appwrite set up commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+import { ThemeProvider } from "next-themes";
+
+import { cn } from "@/lib/utils";
+
+const fontSans = Plus_Jakarta_Sans({ 
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-sans"
+});
+
+export const metadata: Metadata = {
+  title: "Check App Medicare",
+  description: "A healthcare Management System",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+    <body
+      className={cn(
+        "min-h-screen bg-dark-300 font-sans antialiased",
+        fontSans.variable
+      )}
+    >
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        {children}
+      </ThemeProvider>
+    </body>
+  </html>
+  );
+}
